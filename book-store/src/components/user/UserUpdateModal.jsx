cd906d0771453905390d76043f08e74dbeb72bfa
toiphamdev <toiphamasi79@gmail.com>
2023-05-11 23:22:58 +0700

    fix bug

--- book-store/src/components/user/UserUpdateModal.jsx
+++ book-store/src/components/user/UserUpdateModal.jsx
@@ -18,22 +18,20 @@ const UserUpdateModal = ({ open, close, getContainer }) => {
   ];
 
   return (
-    <div style={{ width: "100%", position: "relative", height: "100%" }}>
-      <div style={{ display: "none" }}>
-        <Modal
-          title={"Quản lí tài khoản"}
-          closable={true}
-          cancelButtonProps={true}
-          footer={false}
-          style={{ width: "96%", top: "32px" }}
-          open={open}
-          onCancel={close}
-        >
-          <Row span={24}>
-            <Tabs items={items} defaultActiveKey="1" />
-          </Row>
-        </Modal>
-      </div>
+    <div>
+      <Modal
+        title={"Quản lí tài khoản"}
+        closable={true}
+        cancelButtonProps={true}
+        footer={false}
+        style={{ width: "96%", top: "32px" }}
+        open={open}
+        onCancel={close}
+      >
+        <Row span={24}>
+          <Tabs items={items} defaultActiveKey="1" />
+        </Row>
+      </Modal>
     </div>
   );
 };
